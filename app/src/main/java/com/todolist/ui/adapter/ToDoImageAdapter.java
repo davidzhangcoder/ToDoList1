3f3b54f10aa661ca3c0e348da789c6b82bebe670
dzhang4 <1@hotmail.com>
2019-08-11 19:16:38 -0400

    Feature : Matisse Photo Picker
    (cherry picked from commit f770b3f)

---
+++ app/src/main/java/com/todolist/ui/adapter/ToDoImageAdapter.java
@@ -0,0 +1,49 @@
+package com.todolist.ui.adapter;
+
+import android.content.Context;
+import android.support.annotation.NonNull;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.todolist.R;
+import com.todolist.model.BaseViewHolder;
+import com.todolist.model.ToDoCategoryHodler;
+import com.todolist.model.ToDoImageHolder;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ToDoImageAdapter extends RecyclerView.Adapter<BaseViewHolder> {
+
+    private Context mContext;
+
+    private List<String> data = new ArrayList<String>();
+
+    public ToDoImageAdapter(Context mContext , List<String> data) {
+        this.mContext = mContext;
+        this.data = data;
+    }
+
+    @NonNull
+    @Override
+    public BaseViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(mContext).inflate(R.layout.item_todoimage, parent, false);
+        return new ToDoImageHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull BaseViewHolder baseViewHolder, int position) {
+        baseViewHolder.setUpView( data.get(position) , position , this );
+    }
+
+    @Override
+    public int getItemCount() {
+        return data.size();
+    }
+
+    public Context getContext() {
+        return mContext;
+    }
+}
