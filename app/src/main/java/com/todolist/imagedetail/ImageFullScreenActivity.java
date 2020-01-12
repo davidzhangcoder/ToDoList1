3b8ae33b20e3ddb10427914fab4bff3c04252513
dzhang4 <dzhang@dzhang.com>
2020-01-12 16:18:53 -0500

    Image Enhancement : Add Full Screen Image Functionality, fix some scale and layout issue

--- app/src/main/java/com/todolist/imagedetail/ImageFullScreenActivity.java
+++ app/src/main/java/com/todolist/imagedetail/ImageFullScreenActivity.java
@@ -1,6 +1,7 @@
 package com.todolist.imagedetail;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -8,8 +9,13 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.AppCompatImageView;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
 
 import com.bumptech.glide.Glide;
+import com.bumptech.glide.load.resource.drawable.GlideDrawable;
+import com.bumptech.glide.request.RequestListener;
+import com.bumptech.glide.request.target.Target;
 import com.todolist.R;
 import com.todolist.model.ToDoImage;
 
@@ -39,8 +45,33 @@ public class ImageFullScreenActivity extends AppCompatActivity {
             Glide
                     .with(this.getApplicationContext())
                     .load(toDoImage.getUri())
+                    .listener(new RequestListener<Uri, GlideDrawable>() {
+                        @Override
+                        public boolean onException(Exception e, Uri model, Target<GlideDrawable> target, boolean isFirstResource) {
+                            return false;
+                        }
+
+                        @Override
+                        public boolean onResourceReady(GlideDrawable resource, Uri model, Target<GlideDrawable> target, boolean isFromMemoryCache, boolean isFirstResource) {
+                            if (appCompatImageView == null) {
+                                return false;
+                            }
+                            if (appCompatImageView.getScaleType() != ImageView.ScaleType.FIT_XY) {
+                                appCompatImageView.setScaleType(ImageView.ScaleType.FIT_XY);
+                            }
+                            ViewGroup.LayoutParams params = appCompatImageView.getLayoutParams();
+                            int vw = appCompatImageView.getWidth() - appCompatImageView.getPaddingLeft() - appCompatImageView.getPaddingRight();
+                            float scale = (float) vw / (float) resource.getIntrinsicWidth();
+                            int vh = Math.round(resource.getIntrinsicHeight() * scale);
+                            params.height = vh + appCompatImageView.getPaddingTop() + appCompatImageView.getPaddingBottom();
+                            appCompatImageView.setLayoutParams(params);
+                            return false;
+
+                        }
+                    }
+                    )
 //                    .override(screenWidth, screenHeight)
-                    .centerCrop()
+//                    .centerCrop()
                     .into(appCompatImageView);
         }
 
